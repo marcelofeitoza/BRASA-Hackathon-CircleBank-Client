3503ce04164fb4b3af04e690194cd387ba2c3043
JacksonAguiar <jacksonwellington.sa@gmail.com>
2022-04-03 14:50:09 -0300

    feature(): added Baking settings screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:circlebankapp/screens/BankBalance.dart';
+import 'package:circlebankapp/screens/BankingSettings.dart';
 import 'package:circlebankapp/screens/Home.dart';
 import 'package:circlebankapp/screens/OpenBankingSettings.dart';
 import 'package:flutter/material.dart';
@@ -29,6 +30,6 @@ class MyApp extends StatelessWidget {
         ),
         // home: const HomeScreen(),
         // home: const OpenBankingSettings(),
-        home: const BankBalance());
+        home: const BankingSettings());
   }
 }
